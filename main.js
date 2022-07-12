03eb579d5625071acbf60a91a7e23bffb07cd91c
rrdrumond <rrdrumond@gmail.com>
2022-07-12 23:01:40 +0100

    Add .vscode/settings.json CV/Roberto's Resume.pdf images/My project.png images/PhotoRoberto.jpg images/background.png images/from page.PNG images/homebackground.jpg images/landing page.PNG images/tribute canada.PNG index.html main.js style.css

---
+++ main.js
@@ -0,0 +1,14 @@
+document.querySelector('.menu-btn').addEventListener('click', () => {
+     document.querySelector('.nav-menu').classList.toggle('show');
+});
+
+
+ScrollReveal().reveal('.navbar_logo', { delay: 200 });
+
+ScrollReveal().reveal('.home_description', { delay: 500 });
+ScrollReveal().reveal('.about_description', { delay: 500 });
+ScrollReveal().reveal('.about_skills', { delay: 900 });
+ScrollReveal().reveal('.services', { delay: 800 });
+ScrollReveal().reveal('.projects', { delay: 800 });
+ScrollReveal().reveal('.contact', { delay: 800 });
+ScrollReveal().reveal('.footer', { delay: 800 });
