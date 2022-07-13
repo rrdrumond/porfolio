210e631cdeca1b7ea474383209e538df37bbb366
rrdrumond <rrdrumond@gmail.com>
2022-07-13 21:45:33 +0100

    Add main.

--- main.js
+++ main.js
@@ -7,8 +7,8 @@ ScrollReveal().reveal('.navbar_logo', { delay: 200 });
 
 ScrollReveal().reveal('.home_description', { delay: 500 });
 ScrollReveal().reveal('.about_description', { delay: 500 });
-ScrollReveal().reveal('.about_skills', { delay: 900 });
-ScrollReveal().reveal('.services', { delay: 800 });
-ScrollReveal().reveal('.projects', { delay: 800 });
-ScrollReveal().reveal('.contact', { delay: 800 });
-ScrollReveal().reveal('.footer', { delay: 800 });
+ScrollReveal().reveal('.about_skills', { delay: 500 });
+ScrollReveal().reveal('.services', { delay: 500 });
+ScrollReveal().reveal('.projects', { delay: 500 });
+ScrollReveal().reveal('.contact', { delay: 500 });
+ScrollReveal().reveal('.footer', { delay: 500 });
