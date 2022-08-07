fb9a4a31d0cf02ec40e74a7e49eb8c201f18d706
rrdrumond <rrdrumond@gmail.com>
2022-08-07 21:07:03 +0100

    Add index.hml main.js style.css

--- main.js
+++ main.js
@@ -1,14 +1,18 @@
-document.querySelector('.menu-btn').addEventListener('click', () => {
+/*document.querySelector('.menu-btn').addEventListener('click', () => {
      document.querySelector('.nav-menu').classList.toggle('show');
-});
+});*/
+
+ScrollReveal().reveal('.navbar', {distance: '60px',duration:3000, origin: 'bottom' });
 
 
-ScrollReveal().reveal('.navbar_logo', { delay: 200 });
+ScrollReveal().reveal('.home_description', { distance: '300px',duration:2000, origin: 'top'});
 
-ScrollReveal().reveal('.home_description', { delay: 500 });
-ScrollReveal().reveal('.about_description', { delay: 500 });
-ScrollReveal().reveal('.about_skills', { delay: 500 });
-ScrollReveal().reveal('.services', { delay: 500 });
-ScrollReveal().reveal('.projects', { delay: 500 });
-ScrollReveal().reveal('.contact', { delay: 500 });
-ScrollReveal().reveal('.footer', { delay: 500 });
+ScrollReveal().reveal('.about_description', { distance: '300px',duration:2000, origin: 'left'});
+ScrollReveal().reveal('.about_skills', { delay: 1000, distance: '300px',duration:2000, origin: 'right'});
+ScrollReveal().reveal('.services', { distance: '300px',duration:2000, origin: 'bottom'});
+ScrollReveal().reveal('.projects', { distance: '60px',duration:3000, origin: 'bottom'});
+ScrollReveal().reveal('.contact', { delay: 500, distance: '300px',duration:3000, origin: 'left'});
+ScrollReveal().reveal('.footer', { delay: 1000, duration:4000, origin: 'bottom' });
+
+
+
